0b3c3fee45dbe61e76d14774a0b7b45d4821e138
Kurt Hogentogler <kurt.ah@outlook.com>
2025-09-07 13:39:37 -0600

    Optimize ImageUpload

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Separate TensorFlow.js into its own chunk
+          'tensorflow': ['@tensorflow/tfjs', '@tensorflow-models/mobilenet'],
+          // Separate large UI libraries
+          'ui-libs': [
+            'react-tinder-card',
+            'react-tooltip',
+            '@react-spring/web',
+          ],
+          // Separate utility libraries
+          'utils': ['axios', 'socket.io-client', 'uuid', 'zustand'],
+        },
+      },
+    },
+    // Increase chunk size warning limit since we're optimizing
+    chunkSizeWarningLimit: 1000,
+  },
 })
