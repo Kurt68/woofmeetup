9278300c74d510a0120a2289f7bccd4dc826cb2d
Kurt Hogentogler <kurt.ah@outlook.com>
2025-09-06 22:10:31 -0600

    updated ImageUploadl.jsx

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:8000'],
+    origin: ['https://woofmeetup-1.onrender.com'],
   },
 })
 
