50505cb1b046edc9bf22544780893e8e70507b22
Kurt Hogentogler <kurt.ah@outlook.com>
2025-09-10 12:31:14 -0600

    Refactoring code

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
     origin:
       process.env.NODE_ENV === 'development'
         ? ['http://localhost:5173']
-        : ['https://woofmeetup-1.onrender.com'],
+        : ['https://woofmeetup.com'],
   },
 })
 
