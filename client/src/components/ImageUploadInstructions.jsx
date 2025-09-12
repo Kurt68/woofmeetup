154dc3d6572e6d94a81eef75b73870b5db986ded
Kurt Hogentogler <kurt.ah@outlook.com>
2025-09-12 11:17:36 -0600

    ui tweaks

--- client/src/components/ImageUploadInstructions.jsx
+++ client/src/components/ImageUploadInstructions.jsx
@@ -3,9 +3,9 @@ import { memo } from 'react'
 const ImageUploadInstructions = memo(() => (
   <div>
     <ul className="instructions">
-      <li>&#10003; AI checks an image to match a breed.</li>
-      <li>&#10003; Mixed breed it guesses but knows it&apos;s a dog.</li>
-      <li> &#10003; Head shot & nothing in its mouth works best.</li>
+      <li>&#10003; AI checks an image to match breed.</li>
+      <li>&#10003; Mixed it guesses but knows it&apos;s a dog.</li>
+      <li> &#10003; Head shot & nothing in its mouth.</li>
       &#10003; Portrait pics work best too!
     </ul>
   </div>
