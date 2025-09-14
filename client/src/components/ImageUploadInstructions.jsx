3c8ddfd259697737e4adb3ac9b16dff884feb3a9
Kurt Hogentogler <kurt.ah@outlook.com>
2025-09-14 12:18:46 -0600

    UI tweaks

--- client/src/components/ImageUploadInstructions.jsx
+++ client/src/components/ImageUploadInstructions.jsx
@@ -4,7 +4,7 @@ const ImageUploadInstructions = memo(() => (
   <div>
     <ul className="instructions">
       <li>&#10003; AI checks an image to match breed.</li>
-      <li>&#10003; Mixed it guesses but knows it&apos;s a dog.</li>
+      <li>&#10003; Mixed it guesses.</li>
       <li> &#10003; Head shot & nothing in its mouth.</li>
       &#10003; Portrait pics work best too!
     </ul>
