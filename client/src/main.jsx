6e06117ab29d012f409490fd11b78f1a7681fe09
Kurt Hogentogler <kurt.ah@outlook.com>
2025-11-19 13:59:29 -0700

    SEO integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -30,6 +30,16 @@ async function initializeSentry() {
       // Session Replay
       replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
       replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+      beforeSend(event) {
+        if (
+          event.exception?.values?.[0]?.value?.includes(
+            'className.includes is not a function'
+          )
+        ) {
+          return null
+        }
+        return event
+      },
     })
 
     // Make Sentry available globally for other components
